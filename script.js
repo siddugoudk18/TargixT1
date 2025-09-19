0d840f3bf6d8aa6e74eebc7ef2be83951f186dc5
siddu <siddharthgoud150@gmail.com>
2025-09-19 10:19:55 +0530

    My Task1

--- script.js
+++ script.js
@@ -1,41 +1,30 @@
-const loadBtn=document.getElementById("loadBtn");
-const postsContainer=document.getElementById("postsContainer");
+// const loadBtn = document.getElementById("loadBtn");
+const imageContainer=document.getElementById("postsContainer");
 
-if(loadBtn){
-    loadBtn.addEventListener("click",fetchposts)
-}
+// loadBtn.addEventListener("click", fetchImages);
 
-async function fetchposts(){
-    if(!postsContainer){
-        return;
-    }
-    postsContainer.textContent="There are no posts"
+async function fetchImages() {
+    try {
+        const response = await fetch("https://picsum.photos/v2/list?page=2&limit=12");
+        if (!response.ok) throw new Error("Network response was not ok");
+        const data = await response.json();
+        // console.log(data)
+        // const list=data.message;
+        data.forEach(img=>{
+            const img1=document.createElement("img");
+            img1.src=img.download_url;
+            img1.alt=img.author;
+            img1.style.height="250px";
+            img1.style.width="250px";
+            img1.style.borderRadius="8px";
+            img1.style.padding="2px";
+            // img.classList.add("divi");
+            imageContainer.appendChild(img1);
+        })
 
-    try{
-        const res=await fetch("https://jsonplaceholder.typicode.com/posts?_limit=5");
-        if(!res.ok){
-            throw new Error(`{response.Status}`);
-        }
-        postsContainer.textContent="";
-
-        postsContainer.forEach((p)=>{
-            const div=document.getElementById("div");
-            div.className="post";
-
-            const h3=document.getElementById("h3");
-            h3.textContent=post.title;
-
-            const p=document.getElementById("p");
-            p.textContent=post.body;
-
-            div.append(h3);
-            div.append(p);
-
-            postsContainer.append(div)
-        });
-    }
-    catch(err){
-        postsContainer.textContent="Failed to load posts:`${err.message}`";
-        postsContainer.className="error"
+    } catch (error) {
+        console.log(error);
     }
 }
+fetchImages();
+
